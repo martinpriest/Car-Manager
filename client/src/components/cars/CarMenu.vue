22f35f85b111a7f3713ee71dc0c74494e8d4b7a7
Anna Stankowska <ssstonka@gmail.com>
2020-05-02 22:25:01 +0200

    Added modal with form to add cars

--- client/src/components/cars/CarMenu.vue
+++ client/src/components/cars/CarMenu.vue
@@ -1,10 +1,29 @@
 <template>
-    <h1>Car menu</h1>
+<ul>
+    <li v-for="car in cars" :key ="car.id">{{car.name}}</li>
+    <button id="addCar" @click="addCar = true">Add Car</button>
+    <AddCarModal v-if="addCar" @close="addCar = false">
+        <h3 slot="header">Add new car</h3>
+      </AddCarModal>
+</ul>
 </template>
 
 <script>
+import AddCarModal from './AddCarModal'
 export default {
-    name: 'CarMenu'
+    name: 'CarMenu',
+    components: {
+        AddCarModal
+    },
+    props: {
+        cars: Array,
+        actualCar: Object,
+    },
+    data: function() {
+        return {
+            addCar: false
+        }
+    }
 }
 </script>
 
