d43ea97946caed7aae3a31c7b549d521aa0c2cd9
Kamil689 <kamilkoterla@gmail.com>
2020-05-03 21:55:08 +0200

    Add repair history

---
+++ client/src/components/cars/CarAction/RepairHistory/CarRepairHistory.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="repair-history">
+      <span>Repair history</span>
+      <RepairHistoryTable v-bind:repairHistory="repairHistory"/>
+      <AddRepairHistory v-bind:actualCar="actualCar" v-if="showModal" @close="showModal = false">
+        <h3 slot="header">Add repair record</h3>
+      </AddRepairHistory>
+      <button id="show-modal" @click="showModal = true">Add repair</button>
+  </div>
+</template>
+
+<script>
+import RepairHistoryTable from './RepairHistoryTable';
+import AddRepairHistory from './AddRepairHistory';
+
+export default {
+    name: 'CarRepairHistory',
+    components: {
+      RepairHistoryTable, AddRepairHistory
+    },
+    props: {
+      actualCar: Number
+    },
+    data: function() {
+        return {
+            repairHistory: Array,
+            showModal: false
+        }
+    },
+    created: function() {
+        var json = {
+          idCar: this.actualCar
+        };
+
+        var requestOptions = {
+          method: 'POST',
+          body: JSON.stringify(json),
+          redirect: 'follow',
+          credentials: 'include'
+        };
+
+        fetch(`http://marcin.innome.pl:8000/repair_history/`, requestOptions)
+        .then(response => response.json())
+        .then((result) => {
+          console.log(result);
+          this.repairHistory = result;
+        })
+        .catch(error => console.log('error', error));
+    },
+    watch: {
+      actualCar: function(newVal, oldVal) {
+        console.log('Prop changed: ', newVal, ' | was: ', oldVal);
+        var json = {
+          idCar: this.actualCar
+        };
+
+        var requestOptions = {
+          method: 'POST',
+          body: JSON.stringify(json),
+          redirect: 'follow',
+          credentials: 'include'
+        };
+
+        fetch(`http://marcin.innome.pl:8000/repair_history/`, requestOptions)
+        .then(response => response.json())
+        .then((result) => {
+          console.log(result);
+          // this.repairHistory = result;
+        })
+        .catch(error => console.log('error', error));
+      }
+    }
+}
+</script>
+
+<style>
+  .repair-history {
+    color: white;
+
+  }
+
+  span {
+    color: white;
+  }
+</style>
