e048a25aa4d61abb2cc2e5ecd6fd8ffa1508931c
Kamil689 <kamilkoterla@gmail.com>
2020-05-03 17:42:00 +0200

    Begin implementing dashboard
    - added CarTable and Notification components

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,11 +1,45 @@
 <template>
-<div><h1>Dashboard</h1></div>
-  
+<div class="dashboard">
+    <CarTable v-bind:cars="cars"/>
+<!--    <CostChart/>-->
+    <Notification v-bind:cars="cars"/>
+</div>
 </template>
 
 <script>
+import CarTable from "../components/dashboard/CarTable";
+// import CostChart from "../components/dashboard/CostChart";
+import Notification from "../components/dashboard/Notification";
+
 export default {
-    name: 'Dashboard'
+    name: 'Dashboard',
+    components: {
+        CarTable,
+        // CostChart,
+        Notification
+    },
+    data: function() {
+        return {
+            cars: []
+        }
+    },
+    created: function() {
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow',
+            credentials: 'include'
+        };
+
+        fetch("http://marcin.innome.pl:8000/car", requestOptions)
+            .then(response => response.json())
+            .then((result) => {
+                console.log(result);
+                this.cars = result;
+            })
+            .catch(error => console.log('error', error));
+    },
+    methods: {
+    }
 }
 </script>
 
