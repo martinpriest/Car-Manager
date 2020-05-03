4d12160d0b950c33e7e4fe4f40c40a30726d00e4
Kamil689 <kamilkoterla@gmail.com>
2020-05-03 22:37:14 +0200

    Add fixes before merging

--- client/src/components/cars/CarAction.vue
+++ client/src/components/cars/CarAction.vue
@@ -1,18 +1,26 @@
 <template>
   <div id="car-action">
-      <CarLocalizationHistory v-bind:actualCar="actualCar"/>
-      <CarTankHistory v-bind:actualCar="actualCar"/>
+      <div class="localization-history">
+          <CarLocalizationHistory v-bind:actualCar="actualCar"/>
+      </div>
+      <div class="tank-history">
+          <CarTankHistory v-bind:actualCar="actualCar"/>
+      </div>
+      <div class="repair-history">
+          <CarRepairHistory v-bind:actualCar="actualCar"/>
+      </div>
   </div>
 </template>
 
 <script>
 import CarTankHistory from './CarAction/TankHistory/CarTankHistory'
 import CarLocalizationHistory from './CarAction/LocalizationHistory/CarLocalizationHistory'
+import CarRepairHistory from "./CarAction/RepairHistory/CarRepairHistory";
 
 export default {
     name: 'CarAction',
     components: {
-      CarTankHistory, CarLocalizationHistory
+      CarTankHistory, CarLocalizationHistory, CarRepairHistory
     },
     props: {
       actualCar: Number
@@ -26,7 +34,17 @@ export default {
   flex-direction: row;
   width: 100%;
   height: 100%;
-  align-content: center;
-  align-items: center;
+}
+
+.localization-history {
+    flex: 1;
+}
+
+.tank-history {
+    flex: 1;
+}
+
+.repair-history {
+    flex: 1;
 }
 </style>
