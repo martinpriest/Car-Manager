66d7f9d7304099f477572f2cfa0ab417776642b0
Anna Stankowska <ssstonka@gmail.com>
2020-06-30 20:57:19 +0200

    Change login and register form

--- client/src/views/Logout.vue
+++ client/src/views/Logout.vue
@@ -1,20 +1,177 @@
 <template>
-<div class="app">
+<div class="car-profile-box">
   <div class="logo"><img src="./../assets/logo.png" style="width: 300px" alt=""></div>
-  <div class="container">
-    <LoginForm/>
-    <RegisterForm/>
-  </div>
+    <b-tabs content-class="mt-3 w-100" justified>
+    <b-tab title="Login" active>
+      <b-form @submit="login" @reset="onResetLoginForm" v-if="show">
+      <b-form-group id="input-group-1">
+        <b-form-input
+          id="login-username"
+          v-model="loginForm.login"
+          required
+          placeholder="Login"
+        ></b-form-input>
+      </b-form-group>
+ 
+      <b-form-group id="input-group-2">
+        <b-form-input
+          id="login-password"
+          v-model="loginForm.password"
+          required
+          placeholder="Password"
+          type="password"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    </b-tab>
+    <!-- REGISTER FORM -->
+    <b-tab title="Register">
+<b-form @submit="register" @reset="onResetRegisterForm" v-if="show">
+      <b-form-group id="input-group-3">
+        <b-form-input
+          id="register-login"
+          v-model="registerForm.login"
+          required
+          placeholder="Login"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-4">
+        <b-form-input
+          id="register-mail"
+          v-model="registerForm.mail"
+          required
+          placeholder="Mail"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-5">
+        <b-form-input
+          id="register-password"
+          v-model="registerForm.password"
+          required
+          placeholder="Password"
+          type="password"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-6">
+        <b-form-input
+          id="register-password2"
+          v-model="registerForm.password2"
+          required
+          placeholder="Password"
+          type="password"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+
+
+    </b-tab>
+  </b-tabs>
 </div>
 </template>
 
 <script>
-import LoginForm from './../components/logout/LoginForm'
-import RegisterForm from './../components/logout/RegisterForm'
 export default {
     name: 'Logout',
-    components: {
-      LoginForm, RegisterForm
+    data() {
+      return {
+        loginForm: {
+          login: '',
+          password: ''
+        },
+        registerForm: {
+          login: '',
+          mail: '',
+          password: '',
+          password2: ''
+        },
+        show: true
+      }
+    },
+    methods: {
+      onResetLoginForm(evt) {
+        evt.preventDefault()
+        // Reset our form values
+        this.loginForm.login = ''
+        this.loginForm.password = ''
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      },
+      onResetRegisterForm(evt) {
+        evt.preventDefault()
+        // Reset our form values
+        this.registerForm.login = ''
+        this.registerForm.mail = ''
+        this.registerForm.password = ''
+        this.registerForm.password2 = ''
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      },
+      login: function() {
+        let username = document.querySelector("#login-username");
+        let password = document.querySelector("#login-password");
+
+        var json = {
+          login: username.value,
+          password: password.value
+        };
+
+        var requestOptions = {
+          method: 'POST',
+          body: JSON.stringify(json),
+          redirect: 'follow',
+          credentials: 'include'
+        };
+
+        fetch(`${process.env.VUE_APP_API_URL}/user/login`, requestOptions)
+        .then(response => response.json())
+        .then((data) => {
+          if(data.message == "Logged in") this.$router.push({ path: '/cars' }).catch(err => {console.log(err)})
+          else alert("Nieprawidłowe dane");
+
+          location.reload();
+        })
+        .catch(error => console.log('error', error));
+      },
+      register: function() {
+        let username = document.querySelector("#register-login");
+        let email = document.querySelector("#register-mail");
+        let password = document.querySelector("#register-password");
+        let password2 = document.querySelector("#register-password2");
+        if(password.value != password2.value) {
+          alert("Zle powtorzyles haslo");
+          return 0;
+        }
+        var json = {
+          login: username.value,
+          email: email.value,
+          password: password.value
+        };
+        var requestOptions = {
+          method: 'POST',
+          body: JSON.stringify(json),
+          redirect: 'follow',
+          credentials: 'include'
+        };
+        fetch(`${process.env.VUE_APP_API_URL}/user/create`, requestOptions)
+        .then(response => response.json())
+        .then((result) => {
+          alert(result.message);
+        })
+        .catch(error => console.log('error', error));
+      }
     }
 }
 </script>
@@ -28,24 +185,13 @@ body {
   font-family: sans-serif;
   height: 100vh;
 }
-  .app {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    max-width: 1200px;
-    height: 100vh;
-    margin: 0 auto;
-    align-content: center;
-    align-items: center;
-    /* border: black solid 1px; */
-  }
 
   .logo {
     /* border: black solid 1px; */
   }
 
   .container {
-    
+
     /* border: black solid 1px; */
   }
 </style>
